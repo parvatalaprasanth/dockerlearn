c3da949911cdd9b866f02bafd046a98eb325f708
unknown <parvatala.prasanth.owner@gmail.com>
2023-07-25 02:57:10 +0530

    networking

--- docker-learn.sh
+++ docker-learn.sh
@@ -35,3 +35,54 @@ docker  run -it
 
 #shell inside existing container
 docker exec -it
+
+
+# each container has vpn "bridge"
+# each network has NAT 
+# best VPN for each app
+#  can attach container to app
+#  Can skip network
+
+
+# exposed port
+docker container  port  strange_neumann
+
+
+ docker network ls
+
+ docker network inspect name
+
+ docker network create 
+#  default create bridge
+
+# Can be on multiple networks at a time
+
+ docker network connect networkid containerid
+
+ docker network disconnect ids
+
+# attach nginx to hh network
+  docker run  --network hh nginx
+
+#   docker-Compose spin new network
+
+# --link connection between containers
+
+# container name is dns name
+
+
+# docker commit to save container snapshot
+
+# Automatically remove container after use
+docker run -rm name 
+
+# multiple contains with same dns round robin
+
+# explict dns name
+-network-alias search
+
+# multi container name dns round robin dns
+docker network  create dude
+docker run -d --net dude --net-alias search elasticsearch:2
+docker run -d --net dude --net-alias search elasticsearch:2
+docker  run --rm --net dude alpine nslook search
